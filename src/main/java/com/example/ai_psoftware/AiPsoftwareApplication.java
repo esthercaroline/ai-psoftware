d257b1f5a564032f5033019a642ff3d4e5eee37a
Esther <estherccr@al.insper.edu.br>
2024-09-23 10:40:07 -0300

    build do projeto

---
+++ src/main/java/com/example/ai_psoftware/AiPsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_psoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_psoftware/AiPsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_psoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
